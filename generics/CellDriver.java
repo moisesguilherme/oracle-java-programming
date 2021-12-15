39a8469f03cbd59c3489757875a6a68a2960b64c
Moises Guilherme <moisescomp@gmail.com>
2021-12-15 09:04:18 -0300

    Generic whith two types

--- generics/CellDriver.java
+++ generics/CellDriver.java
@@ -3,14 +3,26 @@ package generics;
 public class CellDriver {
 	
 	public static void main(String[] args) {
-		Cell<Integer> integerCell = new Cell<Integer>();
-		Cell<String> stringCell = new Cell<String>();
-		integerCell.setValue(1);
-		stringCell.setValue("One");
-		int num = integerCell.getValue();
-		String str = stringCell.getValue();
+		Cell<Integer, String> mixCell = new Cell<Integer, String>();
+		Cell<Integer, Integer> integerCell = new Cell<>();
+		
+		
+		mixCell.setValue(1, "4");
+		integerCell.setValue(45, 60);
+		
+		int mcType1 = mixCell.getT1Value();
+		String mcType2 = mixCell.geT2Value();
+		
+		int icType1 = integerCell.getT1Value();
+		int icType2 = integerCell.geT2Value();
 
-		System.out.println("Integer Value: " + num);
-		System.out.println("String Value: " + str);
+		System.out.println(mixCell);
+		System.out.println(integerCell);
+		
+		System.out.println("The numerical value is: " + 
+							mcType1 + ". The text value is: " + mcType2 );
+		
+		System.out.println("The numerical value is: " + 
+				icType1 + ". and the second is: " + mcType2 );
 	}
 }
