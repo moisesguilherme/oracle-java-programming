5c06fa32e634129407b650d313611abba3323113
Moises Guilherme <moisescomp@gmail.com>
2021-12-23 11:35:00 -0300

    Bubble Sort

--- collections/sorting/examples/SortExample.java
+++ collections/sorting/examples/SortExample.java
@@ -7,9 +7,9 @@ public class SortExample {
 	public static void main(String[] args) {
 		int[] numbers = {40, 7, 59, 4, 1};
 		displayValues(numbers);
-		selectionSort(numbers);
+		//selectionSort(numbers);
+		bubbleSort(numbers);
 		displayValues(numbers);
-		
 	}
 	
 	static void selectionSort(int[] numbers) {
@@ -24,6 +24,23 @@ public class SortExample {
 			int temp = numbers[i];
 			numbers[i] = numbers[indexMin];
 			numbers[indexMin] = temp;
+		}
+	}
+	
+	static void bubbleSort(int[] numbers) {
+		boolean flag = false;
+		for(int i = 0; i < numbers.length; i++) {
+			for(int j = 0; j < numbers.length-1; j++) {
+				if(numbers[j] > numbers[j +1]) {
+					flag = true;
+					int temp = numbers[j];
+					numbers[j] = numbers[j + 1];
+					numbers[j + 1] = temp;
+				}
+			}
+			if(!flag) {
+				return;
+			}
 		}
 	}
 	
