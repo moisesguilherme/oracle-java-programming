93cda74ae146a84bb7d53569dbc3ca6c8c336cf8
Moises Guilherme <moisescomp@gmail.com>
2022-01-09 12:14:14 -0300

    Using Square Brackets, Hyphen and Dot

--- regularexpressions/regularexpressionexample.java
+++ regularexpressions/regularexpressionexample.java
@@ -3,16 +3,29 @@ package regularexpressions;
 public class regularexpressionexample {
 
 	public static void main(String[] args) {
-		if(getAnimal("dog"))
+		/*if(getAnimal("dog"))
 		   System.out.println("This is a Valid Animal");
 		else
 		   System.out.println("This is a Valid Animal");
-
+		*/
+		String str = "9a";
+		System.out.println(viewIfMatch(str));
+		
+		System.out.println(anyCharacter(str));
 	}
 	
 	public static boolean getAnimal(String animal) {
 		//return animal.matches("cat|dog|duck");
 		return animal.matches("[Cc]at|[Dd]og");
+	}
+	
+	public static boolean viewIfMatch(String str) {
+		//space before 0
+		return str.matches("[ 0-9a-zA-Zbcde]");
+	}
+	
+	public static boolean anyCharacter(String str) {
+		return str.matches("[0-9].");
 	}
 
 }
